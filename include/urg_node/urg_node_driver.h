471b222bd6931d5f3c2fb04006f5c3f69a90d914
AufarZakiev <zaufar@gmail.com>
2018-01-10 17:37:58 +0300

    Server and client connection establish and messages sent.

--- include/urg_node/urg_node_driver.h
+++ include/urg_node/urg_node_driver.h
@@ -46,91 +46,107 @@
 
 #include "urg_node/urg_c_wrapper.h"
 
-namespace urg_node
-{
-class UrgNode
-{
-public:
-  UrgNode();
-  UrgNode(ros::NodeHandle nh, ros::NodeHandle private_nh);
-  ~UrgNode();
+#include <ctime>
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
 
-  /**
-   * @brief Start's the nodes threads to run the lidar.
-   */
-  void run();
+namespace urg_node {
+    class UrgNode {
+    public:
+        UrgNode(boost::asio::io_service &io_service);
 
-  /**
-   * @brief Trigger an update of the lidar's status
-   * publish the latest known information about the lidar on latched topic.
-   * @return True on update successful, false otherwise.
-   */
-  bool updateStatus();
+        UrgNode(ros::NodeHandle nh, ros::NodeHandle private_nh, boost::asio::io_service &io_service);
 
-private:
-  void initSetup();
-  bool connect();
-  bool reconfigure_callback(urg_node::URGConfig& config, int level);
-  void update_reconfigure_limits();
-  void calibrate_time_offset();
-  void updateDiagnostics();
-  void populateDiagnosticsStatus(diagnostic_updater::DiagnosticStatusWrapper &stat);
-  void scanThread();
+        ~UrgNode();
 
-  bool statusCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
+        /**
+         * @brief Start's the nodes threads to run the lidar.
+         */
+        void run();
 
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_;
+        /**
+         * @brief Trigger an update of the lidar's status
+         * publish the latest known information about the lidar on latched topic.
+         * @return True on update successful, false otherwise.
+         */
+        bool updateStatus();
 
-  boost::thread diagnostics_thread_;
-  boost::thread scan_thread_;
+    private:
+        void initSetup();
 
-  boost::shared_ptr<urg_node::URGCWrapper> urg_;
-  boost::shared_ptr<dynamic_reconfigure::Server<urg_node::URGConfig> > srv_;  ///< Dynamic reconfigure server
-  boost::shared_ptr<diagnostic_updater::Updater> diagnostic_updater_;
-  boost::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic> laser_freq_;
-  boost::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic> echoes_freq_;
+        bool connect();
 
-  boost::mutex lidar_mutex_;
+        bool reconfigure_callback(urg_node::URGConfig &config, int level);
 
-  /* Non-const device properties.  If you poll the driver for these
-  * while scanning is running, then the scan will probably fail.
-  */
-  std::string device_status_;
-  std::string vendor_name_;
-  std::string product_name_;
-  std::string firmware_version_;
-  std::string firmware_date_;
-  std::string protocol_version_;
-  std::string device_id_;
-  uint16_t error_code_;
-  bool lockout_status_;
+        void update_reconfigure_limits();
 
-  int error_count_;
-  double freq_min_;
-  bool close_diagnostics_;
-  bool close_scan_;
+        void calibrate_time_offset();
 
-  int ip_port_;
-  std::string ip_address_;
-  std::string serial_port_;
-  int serial_baud_;
-  bool calibrate_time_;
-  bool publish_intensity_;
-  bool publish_multiecho_;
-  int error_limit_;
-  double diagnostics_tolerance_;
-  double diagnostics_window_time_;
-  bool detailed_status_;
+        void updateDiagnostics();
 
-  volatile bool service_yield_;
+        void populateDiagnosticsStatus(diagnostic_updater::DiagnosticStatusWrapper &stat);
 
-  ros::Publisher laser_pub_;
-  laser_proc::LaserPublisher echoes_pub_;
-  ros::Publisher status_pub_;
+        void scanThread();
 
-  ros::ServiceServer status_service_;
-};
+        bool statusCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
+
+        ros::NodeHandle nh_;
+        ros::NodeHandle pnh_;
+
+        boost::thread diagnostics_thread_;
+        boost::thread scan_thread_;
+
+        boost::shared_ptr<urg_node::URGCWrapper> urg_;
+        boost::shared_ptr<dynamic_reconfigure::Server<urg_node::URGConfig> > srv_;  ///< Dynamic reconfigure server
+        boost::shared_ptr<diagnostic_updater::Updater> diagnostic_updater_;
+        boost::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic> laser_freq_;
+        boost::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic> echoes_freq_;
+
+        boost::mutex lidar_mutex_;
+
+        /* Non-const device properties.  If you poll the driver for these
+        * while scanning is running, then the scan will probably fail.
+        */
+        std::string device_status_;
+        std::string vendor_name_;
+        std::string product_name_;
+        std::string firmware_version_;
+        std::string firmware_date_;
+        std::string protocol_version_;
+        std::string device_id_;
+        uint16_t error_code_;
+        bool lockout_status_;
+
+        int error_count_;
+        double freq_min_;
+        bool close_diagnostics_;
+        bool close_scan_;
+
+        int ip_port_;
+        std::string ip_address_;
+        std::string serial_port_;
+        int serial_baud_;
+        bool calibrate_time_;
+        bool publish_intensity_;
+        bool publish_multiecho_;
+        int error_limit_;
+        double diagnostics_tolerance_;
+        double diagnostics_window_time_;
+        bool detailed_status_;
+
+        volatile bool service_yield_;
+
+        ros::Publisher laser_pub_;
+        laser_proc::LaserPublisher echoes_pub_;
+        ros::Publisher status_pub_;
+
+        ros::ServiceServer status_service_;
+
+        // UDP server objects
+        boost::asio::ip::udp::socket socket_;
+        boost::asio::ip::udp::endpoint remote_endpoint_;
+    };
 
 }  // namespace urg_node
 
--- src/urg_node.cpp
+++ src/urg_node.cpp
@@ -33,15 +33,15 @@
 
 #include "urg_node/urg_node_driver.h"
 
-int main(int argc, char **argv)
-{
-  // Initialize node and nodehandles
-  ros::init(argc, argv, "urg_node");
+int main(int argc, char **argv) {
+    // Initialize node and nodehandles
+    ros::init(argc, argv, "urg_node");
+    boost::asio::io_service io_service;
 
-  urg_node::UrgNode node;
-  node.run();
+    urg_node::UrgNode node(io_service);
+    node.run();
 
-  ros::spin();
+    ros::spin();
 
-  return 0;
+    return 0;
 }
